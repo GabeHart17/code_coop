25259df539e3713218b9555eead437490fdd0f7c
Gabe Hart <flyingmessier@gmail.com>
2021-01-23 14:31:00 -0500

    challenge route

--- application.py
+++ application.py
@@ -1,14 +1,26 @@
+import os
+import psycopg2
 from flask import Flask, render_template, request
+from scripts/db_manager import DBManager
 
 app = Flask(__name__)
+
+dbmgr = DBManager(os.environ['DATABASE_URL'])
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/exercise', methods=['GET'])
-def exercise():
-    id = request.args.get('e')
+@app.route('/challenge', methods=['GET'])
+def challenge():
+    id = request.args.get('c')
+    ch = dbmgr.get_challenge_by_id(id)
+    return render_template('challenge.html',
+        title=ch.title,
+        description=ch.description,
+        instructions=ch.instructions,
+        shown_tests=list(filter(lambda x: x.visible, ch.test_cases)),
+        hidden_tests=list(filter(lambda x: !x.visible, ch.test_cases)))
 
 
 if __name__ == '__main__':
