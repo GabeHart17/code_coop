687575303b2698e1535ea5b321e061eb63a944b2
Ben Gordon <benjamintheepic@gmail.com>
2021-01-24 10:08:30 -0500

    almost done

--- scripts/database_manager.py
+++ scripts/database_manager.py
@@ -27,13 +27,31 @@ class DatabaseManager:
         test_case.id = self.new_testcase_id()
         self.cur.execute(f"INSERT INTO TestCases VALUES ({test_case.id}, {chal_id}, {test_case.shown}, '{test_case.specified_input}', '{test_case.specified_output}');")
         conn.commit()
+    def delete_testcase(self, case_id):
+        self.cur.execute(f"DELETE FROM TestCases WHERE id={case_id};")
+        conn.commit()
+    def update_testcase(self, case):
+        self.cur.execute(f"UPDATE TestCases SET shown={case.shown}, input='{chal.specified_input}', output='{chal.specified_output}' WHERE id={case.case_id};");
+        conn.commit()
     def delete_challenge(self, chal_id):
         self.cur.execute(f"DELETE FROM Challenges WHERE id={chal_id};")
         self.cur.execute(f"DELETE FROM TestCases WHERE challenge_id={chal_id};")
         conn.commit()
-    def update_challenge(self, chal):
+    def update_challenge(self, chal): #may not be a great method to call
         self.cur.execute(f"UPDATE Challenges SET title='{chal.title}', author_id={chal.author_id}, description='{chal.desc}', instructions='{chal.instructions}' WHERE id={chal.id};");
+        self.cur.execute(f"SELECT * FROM TestCases WHERE challenge_id={chal.id};")
+        db_data = cur.fetchall()
+        chal_case_ids = [x.case_id for x in chal.test_cases]
+        db_case_ids = [x[1] for x in db_data]
         
+        for case_id in db_case_ids: #delete removed cases
+            if case_id not in chal_case_ids:
+                self.delete_testcase(case_id)
+        for i in range(0, len(chal_case_ids)): # add new cases
+            if chal_case_ids[i] not in db_case_ids:
+                self.add_testcase(chal.id, chal.test_cases[i])
+            else:
+                self.update_testcase(chal.test_cases[i])
         conn.commit()
     def get_challenge_by_id(self, chal_id):
         self.cur.execute(f"SELECT * FROM Challenges WHERE id={chal_id};")
@@ -44,8 +62,13 @@ class DatabaseManager:
             chal_data[-1].append(TestCase(*tuple(case[:1]+case[2:])))
         return Challenge(*tuple(chal_data)) 
     def get_challenges_by_user(self, user_id):
-        pass
-    def get_challenges_by_kewords(self, search_term):
+        self.cur.execute(f"SELECT * FROM Challenges WHERE author_id={user_id};")
+        chals = self.cur.fetchall()
+        out = []
+        for chal_id in [x[0] for x in chals]:
+            out.append(self.get_challenge_by_id(chal_id))
+        return out
+    def get_challenges_by_keywords(self, search_term):
         pass
 
 class Challenge:
