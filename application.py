3b84e37de59d2966826b6c7a2f4a74b9d9a42814
Gabe Hart <flyingmessier@gmail.com>
2021-01-24 10:02:11 -0500

    cleanup

--- application.py
+++ application.py
@@ -4,12 +4,10 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 import werkzeug.security as ws
 from scripts.users import User, UserManager
-# from scripts/database_manager import DatabaseManager, Challenge, TestCase
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-# db_mgr = DatabaseManager(os.environ['DATABASE_URL'])
 usr_mgr = UserManager(os.environ['DATABASE_URL'])
 login_mgr = LoginManager()
 login_mgr.init_app(app)
@@ -21,17 +19,6 @@ def load_user(uid):
 @app.route('/')
 def home():
     return render_template('index.html')
-
-# @app.route('/challenge', methods=['GET'])
-# def challenge():
-#     id = request.args.get('c')
-#     ch = dbmgr.get_challenge_by_id(id)
-#     return render_template('challenge.html',
-#         title=ch.title,
-#         description=ch.description,
-#         instructions=ch.instructions,
-#         shown_tests=list(filter(lambda x: x.visible, ch.test_cases)),
-#         hidden_tests=list(filter(lambda x: not x.visible, ch.test_cases)))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
