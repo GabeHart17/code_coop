9104f2af0efad0821649dc5f58393214a1e063f7
Ben Gordon <benjamintheepic@gmail.com>
2021-01-23 19:13:20 -0500

    further skidding

--- scripts/database_manager.py
+++ scripts/database_manager.py
@@ -1,18 +1,26 @@
 import psycopg2
 
-user_id = 0
-
 class DatabaseManager:
-    def __init__(self, conn_data):
-        self.conn = psycopg2.connect(conn_data)
-        self.cur = conn.cursor()
-    def get_number_of_challenges(self):
+    def __init__(self, conn):
+        self.conn = conn;
+        self.cur = self.conn.cursor()
+    def get_challenge_count(self):
         self.cur.execute("SELECT COUNT(id) FROM Challenges")
         return self.cur.fetchone()[0]
-
-    def add_challenge(chal):
-        chal.id = self.get_number_of_challenges()
-        self.cur.execute(f"INSERT INTO Challenges VALUES ({chal.id}, {chal.title}, {chal.author_id}, {chal.desc}, {chal.instructions});")
+    def get_testcase_count(self):
+        self.cur.execute("SELECT COUNT(id) FROM TestCases")
+        return self.cur.fetchone()[0]
+    def add_challenge(self, chal):
+        chal.id = self.get_challenge_count()
+        self.cur.execute("INSERT INTO Challenges (id, title, author_id, description, instructions) VALUES (%s, '%s', %s, '%s', '%s');"
+         % (chal.id, chal.title, chal.author_id, chal.desc, chal.instructions))
+        current_casecount = self.get_testcase_count()
+        for case in chal.test_cases:
+            self.add_testcase(chal.id, case)
+        conn.commit()
+    def add_testcase(self, chal_id, test_case):
+        test_case.id = self.get_testcase_count()
+        self.cur.execute(f"INSERT INTO TestCases VALUES ({test_case.id}, {chal_id}, {test_case.shown}, '{test_case.specified_input}', '{test_case.specified_output}');")
 
     def delete_challenge(chal):
         pass
@@ -33,8 +41,8 @@ class Challenge:
         self.desc = desc
         self.instructions = instructions
         self.test_cases = test_cases
-    def new_test_case(shown, specified_input, specified_output):
-        self.test_cases.append(-1, shown, specified_input, specified_output))
+    def new_test_case(self, shown, specified_input, specified_output):
+        self.test_cases.append(TestCase(-1, shown, specified_input, specified_output))
 
 class TestCase:
     def __init__(self, case_id, shown, specified_input, specified_output):
@@ -42,3 +50,20 @@ class TestCase:
         self.shown = shown
         self.specified_input = specified_input
         self.specified_output = specified_output
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("dbname=mydb user=benjabob317")
+    manager = DatabaseManager(conn)
+    
+    factorial = Challenge(
+        -1,
+        "Factorial",
+        0,
+        "For a given integer n return n(n-1)(n-2)...*1",
+        "Make a function that finds the factorial of an integer.\n Recursive or loop variants are permitted."
+    )
+    factorial.new_test_case(True, "5", "120")
+    factorial.new_test_case(True, "0", "1")
+    factorial.new_test_case(False, "10", "3628800")
+    
+    manager.add_challenge(factorial)
