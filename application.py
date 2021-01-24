d5f4aac5ccd91cc7c7ad72f7e10f63e4a8acc1aa
Gabe Hart <flyingmessier@gmail.com>
2021-01-23 23:28:21 -0500

    work on user system

--- application.py
+++ application.py
@@ -1,39 +1,71 @@
 import os
 import psycopg2
-from flask import Flask, render_template, request
-from scripts/database_manager import DatabaseManager, Challenge, TestCase
-import werkzeug.security as ws
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager
+from scripts.users import User, UserManager
+# from scripts/database_manager import DatabaseManager, Challenge, TestCase
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-db_mgr = DatabaseManager(os.environ['DATABASE_URL'])
+# db_mgr = DatabaseManager(os.environ['DATABASE_URL'])
 usr_mgr = UserManager(os.environ['DATABASE_URL'])
 login_mgr = LoginManager()
 login_mgr.init_app(app)
+
+@login_mgr.user_loader
+def load_user(uid):
+    return usr_mgr.get_user_by_id(uid)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/challenge', methods=['GET'])
-def challenge():
-    id = request.args.get('c')
-    ch = dbmgr.get_challenge_by_id(id)
-    return render_template('challenge.html',
-        title=ch.title,
-        description=ch.description,
-        instructions=ch.instructions,
-        shown_tests=list(filter(lambda x: x.visible, ch.test_cases)),
-        hidden_tests=list(filter(lambda x: !x.visible, ch.test_cases)))
+# @app.route('/challenge', methods=['GET'])
+# def challenge():
+#     id = request.args.get('c')
+#     ch = dbmgr.get_challenge_by_id(id)
+#     return render_template('challenge.html',
+#         title=ch.title,
+#         description=ch.description,
+#         instructions=ch.instructions,
+#         shown_tests=list(filter(lambda x: x.visible, ch.test_cases)),
+#         hidden_tests=list(filter(lambda x: not x.visible, ch.test_cases)))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        return render_template('login.html')
     uname = request.form['username']
-    psk = request.form['password']
-    p_hash = ws.generate_password_hash(psk)
-    # get usr
+    usr = usr_mgr.get_user_by_name(uname)
+    err = ''
+    if usr is not None:
+        psk = request.form['password']
+        if (usr_mgr.authenticate_user(usr, psk)):
+            login_user(usr)
+            return redirect('/index')
+        else:
+            err = 'invalid_credentials'
+    else:
+        err = 'no such user'
+    return render_template('login.html', error=err)
 
-
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    err = ''
+    uname = request.form['username']
+    psk0 = request.form['pasword']
+    psk1 = request.form['confirmpassword']
+    if ws.safe_str_cmp(psk0, psk1):
+        if usr_mgr.create_user(uname, psk0):
+            return redirect('login')
+        else:
+            err = 'username taken'
+    else:
+        err = 'passwords do not match'
+    return render_template('register.html', error=err)
 
 if __name__ == '__main__':
     app.run()
--- scripts/users.py
+++ scripts/users.py
@@ -1,21 +1,42 @@
 import psycopg2
+import werkzeug.security as ws
 
 class UserManager:
     def __init__(self, db_url):
-        self.conn = psycopg2.connect("dbname=codecoop_users user=postgres")
+        self.conn = psycopg2.connect("dbname=codecoop user=postgres password=postgres host=localhost")
         self.cur = self.conn.cursor()
 
     def get_user_by_name(self, uname):
-        cur.execute("SELECT * FROM users WHERE username=%s", (uname,))
-        u = cur.fetchone()
+        self.cur.execute("SELECT * FROM users WHERE username=%s", (uname,))
+        u = self.cur.fetchone()
         if u is None: return u
-        return User(u[0], u[1])
+        return User(u[0], u[1], hash=u[2])
+
+    def get_user_by_id(self, uid):
+        self.cur.execute("SELECT * FROM users WHERE id=%s", (uid,))
+        u = self.cur.fetchone()
+        if u is None: return u
+        return User(u[0], u[1], hash=u[2])
+
+    # returns true if user was successfully created, false if username taken
+    def create_user(self, uname, psk):
+        if self.get_user_by_name(uname) is not None:
+            return False
+        hash = ws.generate_password_hash(psk)
+        self.cur.execute("INSERT INTO users (username, hash) VALUES (%s, %s)", (uname, hash))
+        return True
+
+    def authenticate_user(self, usr, psk):
+        usr_info = self.get_user_by_name(usr.uname)
+        usr.auth = ws.check_password_hash(usr.hash, psk)
+        return usr.auth
 
 
 class User:
-    def __init__(self, uid, uname):
-        self.uid = uid
-        self.uname = uname;
+    def __init__(self, uid, uname, hash=''):
+        self.uid = str(uid)
+        self.uname = uname
+        self.hash = hash
         self.auth = False
 
     def is_authenticated(self):
